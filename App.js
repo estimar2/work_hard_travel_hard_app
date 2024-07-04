63041cdc200ae144d09eb0e10337497c1872298e
estimar2 <songsm5504@gmail.com>
2024-07-04 14:09:10 +0900

    [24.07.04] #3.2 TextInput

--- App.js
+++ App.js
@@ -13,31 +13,46 @@ import { theme } from "./colors";
 
 export default function App() {
   const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  const onChangeText = payload => setText(payload);
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
       <View style={styles.header}>
-        <TouchableOpacity
-          onPress={() => {
-            console.log("TouchableOpacity");
-          }}
-        >
+        <TouchableOpacity onPress={work}>
           <Text
             style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
           >
             Work
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log("TouchableHighlight")}>
+        <TouchableOpacity onPress={travel}>
           <Text
-            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
           >
             Travel
           </Text>
         </TouchableOpacity>
       </View>
+
+      <TextInput
+        style={styles.input}
+        returnKeyType="done"
+        placeholder={
+          working ? "what do you have to do?" : "Where do you want to go?"
+        }
+        value={text}
+        onChangeText={onChangeText}
+      />
     </View>
   );
 }
@@ -57,6 +72,13 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: "600",
-    color: theme.grey,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
   },
 });
